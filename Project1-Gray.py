3d836d901dd1d95035b11dee0324c430506261ed
Monniecx <monniecx@gmail.com>
2025-06-29 21:19:06 -0500

    Week 1-4 assigments
    Added all projects and documents for weeks 1-4

---
+++ Project1-Gray.py
@@ -0,0 +1,13 @@
+Python 3.13.4 (tags/v3.13.4:8a526ec, Jun  3 2025, 17:46:04) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> name = 'Monica Gray'
+>>> address = '616 W jefferson ave, Checotah, Oklahoma 74426"
+SyntaxError: unterminated string literal (detected at line 1)
+>>> address = '616 W jefferson ave, Checotah, Oklahoma 74426'
+>>> Phone Number = '918-840-3437'
+SyntaxError: invalid syntax
+>>> phone number = '918-840-3437'
+SyntaxError: invalid syntax
+>>> phone_number = '918-840-3437'
+>>> print(name, address, phone_number)
+Monica Gray 616 W jefferson ave, Checotah, Oklahoma 74426 918-840-3437
---
+++ Project3-Gray.py
@@ -0,0 +1,8 @@
+Python 3.13.4 (tags/v3.13.4:8a526ec, Jun  3 2025, 17:46:04) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> base = float(input("Enter the base: "))
+Enter the base: 6
+>>> height = float(input("Enter the height"))
+Enter the height 9
+>>> print(.5 * base * height)
+27.0
